dd86cbcc1af8ee502af1aeb7bfa3b5c336ff35bc
Dominic Luidold <DominicLuidold@users.noreply.github.com>
2023-05-19 19:56:41 +0200

    Install vim as Git dependency

--- scripts/git.sh
+++ scripts/git.sh
@@ -1,5 +1,9 @@
 #!/bin/bash -e
 
+# Dependencies
+echo "# Installing Git dependencies ..."
+sudo apt install -qqy vim
+
 # Copies & symlinks
 echo "# Copying Git config files ..."
 cp -u ~/.dotfiles/git/.gitconfig ~/.gitconfig
