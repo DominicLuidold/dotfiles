3909bdceaa45bfb70a785a84ccfcd8641e52493b
Dominic Luidold <DominicLuidold@users.noreply.github.com>
2023-05-19 20:26:02 +0200

    Add download of external certificates

--- scripts/ssh.sh
+++ scripts/ssh.sh
@@ -4,3 +4,7 @@
 echo "# Creating ssh symlinks & copying ssh config files ..."
 ln -sf ~/.dotfiles/ssh/allowed_signers ~/.ssh/allowed_signers
 cp -u ~/.dotfiles/ssh/config ~/.ssh/config
+
+# External certificates
+echo "# Downloading external certificates ..."
+curl -fsSL https://truststore.pki.rds.amazonaws.com/eu-central-1/eu-central-1-bundle.pem > ~/.ssh/eu-central-1-bundle.pem
