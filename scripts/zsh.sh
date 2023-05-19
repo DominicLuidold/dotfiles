bdce4f492219bf0ab066eda7a86f371f75c7bbc8
Dominic Luidold <DominicLuidold@users.noreply.github.com>
2023-05-19 20:44:09 +0200

    Only install Oh My Zsh if not already installed

--- scripts/zsh.sh
+++ scripts/zsh.sh
@@ -1,17 +1,29 @@
 #!/bin/bash -e
 
+GREEN="\033[1;32m"
+NO_COLOR="\033[0m"
+
 # Install
-echo "# Installing zsh ..."
+echo -e "${GREEN}# Installing zsh ...${NO_COLOR}"
 sudo apt install -qqy zsh
 chsh -s $(which zsh)
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 
-# Dependencies
-echo "# Installing zsh dependencies ..."
-sudo apt install -qqy fonts-powerline
+if [ -d ~/.oh-my-zsh ]; then
+    # Skip if Oh My Zsh is already installed
+    echo -e "${GREEN}# Skipping Oh My Zsh installation - already installed${NO_COLOR}"
+    exit 0
+else
+    # Install Oh My Zsh
+    echo -e "${GREEN}# Installing Oh My Zsh ...${NO_COLOR}"
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+
+    # Dependencies
+    echo -e "${GREEN}# Installing Oh My Zsh dependencies ...${NO_COLOR}"
+    sudo apt install -qqy fonts-powerline
+fi
 
 # Symlinks
-echo "# Creating zsh symlinks ..."
+echo -e "${GREEN}# Creating Oh My Zsh symlinks ...${NO_COLOR}"
 ln -sf ~/.dotfiles/zsh/.zshrc ~/.zshrc
 ln -sf ~/.dotfiles/zsh/1password.zsh ~/.oh-my-zsh/custom/1password.zsh
 ln -sf ~/.dotfiles/zsh/aliases.zsh ~/.oh-my-zsh/custom/aliases.zsh
