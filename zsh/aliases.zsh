d9fdfc5b19b9903f4b3541a1c7ac7e6678908ccc
Dominic Luidold <DominicLuidold@users.noreply.github.com>
2023-06-30 14:20:46 +0200

    Add aliases for more commonly used commands

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -5,3 +5,7 @@ alias dpsa="docker ps -a --format \"table {{.ID}}\t{{.Status}}\t{{.Names}}\t{{.R
 
 ## Git
 alias git-housekeeping="git fetch -p && for branch in `git branch -vv | grep ': gone]' | awk '{print $1}'`; do git branch -D $branch; done"
+alias gitlab-mr="glab mr create --remove-source-branch --squash-before-merge --push"
+
+## Misc
+alias fix-network="sudo systemctl restart NetworkManager"
