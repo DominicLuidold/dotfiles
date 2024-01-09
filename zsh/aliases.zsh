9c385df70ad5cbf66d6f82cad08917fd27b19d94
Dominic Luidold <DominicLuidold@users.noreply.github.com>
2024-01-09 08:11:04 +0100

    Improve Git housekeeping alias

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -4,7 +4,7 @@
 alias dpsa="docker ps -a --format \"table {{.ID}}\t{{.Status}}\t{{.Names}}\t{{.RunningFor}}\t{{.Ports}}\""
 
 ## Git
-alias git-housekeeping="git fetch -p && for branch in `git branch -vv | grep ': gone]' | awk '{print $1}'`; do git branch -D $branch; done"
+alias git-housekeeping="git fetch -p && for branch in $(git branch -vv | grep ': gone]' | awk '{print $1}'); do git branch -D $branch; done"
 alias gitlab-mr="glab mr create --remove-source-branch --squash-before-merge --push"
 
 ## Misc
